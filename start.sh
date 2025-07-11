83641427f9eb591c6ec2008e0276a88e535531ec
daryllundy <daryl.lundy@gmail.com>
2025-07-10 19:12:39 -0700

    fix: update docker-compose to use FREE_PORT env var created by start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 get_free_port() {
-  comm -23 <(seq 8000 8100 | sort) <(ss -Htan | awk '{print $4}' | grep -oE '[0-9]+$' | sort -n | uniq) | head -n 1
+  comm -23 <(seq 3000 3100 | sort) <(ss -Htan | awk '{print $4}' | grep -oE '[0-9]+$' | sort -n | uniq) | head -n 1
 }
 
 PORT=$(get_free_port)
