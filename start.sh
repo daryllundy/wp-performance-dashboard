c00993a6bf29afd79e30d80be6370bbec80b8ff3
daryllundy <daryl.lundy@gmail.com>
2025-07-10 19:07:38 -0700

    feat: create start.sh to auto find free port for use with docker compose

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+get_free_port() {
+  comm -23 <(seq 8000 8100 | sort) <(ss -Htan | awk '{print $4}' | grep -oE '[0-9]+$' | sort -n | uniq) | head -n 1
+}
+
+PORT=$(get_free_port)
+echo "Using free port: $PORT"
+export FREE_PORT=$PORT
+docker-compose up
